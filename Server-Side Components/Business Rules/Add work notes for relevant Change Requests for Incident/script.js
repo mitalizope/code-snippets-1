c3a60ace9cf2a86392b2eda66672fcbd95b2adc9
mits <104266846+mitalizope@users.noreply.github.com>
2025-10-03 00:17:45 +0530

    Update script.js
    This Business Rule checks for Change Requests linked to the same Configuration Item (CI) whenever an Incident is created or updated.  
    If any active CRs are found (excluding Closed and Canceled), their numbers are added to the Incident’s work notes.  
    This helps agents identify related changes quickly and avoid duplicate effort.

--- Server-Side Components/Business Rules/Add work notes for relevant Change Requests for Incident/script.js
+++ Server-Side Components/Business Rules/Add work notes for relevant Change Requests for Incident/script.js
@@ -1,8 +1,7 @@
 (function executeRule(current, previous /*null when async*/) {
 
-	// Add your code here
 	if(current.cmdb_ci){
-		var ci = current.cmdb_ci;
+		var ci = current.cmdb_ci.getValue();
 
 		var chng = new GlideRecord('change_request');
 		chng.addQuery('cmdb_ci', ci);
@@ -10,14 +9,16 @@
 		chng.addQuery('state', '!=', '4');
 		chng.query();
 
-		var work_notes = 'Following change requests are associated with same CI. You can attach one of them.\n';
+		var work_notes = '';
 
 		while(chng.next()){
-			work_notes += chng.number + '\n';
+			work_notes += chng.getValue('number') + '\n';
 		}
 
-		current.work_notes = work_notes;
-		current.update();
+		if(work_notes){
+			current.work_notes = 'Following change requests are associated with the same CI. You can attach one of them.\n' + work_notes;
+		}
+	
 	}
 
 })(current, previous);
